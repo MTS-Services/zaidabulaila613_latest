309f054f6c6ede9e1dc6ef04946cbc14a73f80b0
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-09-17 16:32:01 +0600

    (fix): forgot-password page fixed

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,10 +1,10 @@
-import ForgetPassword from '@/interfaces/auth/forgotPassword';
+import ForgotPasswordPage from '@/interfaces/auth/forgotPassword';
 import { Suspense } from 'react';
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading ...</div>}>
-      <ForgetPassword />
+      <ForgotPasswordPage />
     </Suspense>
   );
 }
